2a9139a22372add4009b6433813d6b6aac45fd8f
callmeniyu <getniyashere@gmail.com>
2024-04-07 21:28:05 +0530

    Admin pages set up sidebar ready admin addproduct ready

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
-import Navbar from "./Components/Navbar/Navbar"
+import Navbar from "./Components/Navbar/Navbar" 
+import Admin from "./Pages/Admin/Admin"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <Admin />
     </>
   )
 }
