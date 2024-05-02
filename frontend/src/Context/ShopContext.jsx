f27e3b848618758bc9720cb20823185655b3e97d
callmeniyu <getniyashere@gmail.com>
2024-05-02 07:25:41 +0530

    "Products are fetched from database and displayed.
    -Updated category routes to lowercase, fetched product data from API, and updated padding in LoginSignup.css"
    
    Explanation:
    
    * The category routes in App.jsx have been updated to use lowercase for

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -1,18 +1,37 @@
 import React, { createContext, useEffect, useState } from "react"
-import all_product from "../Components/Assets/all_product"
+// import all_product from "../Components/Assets/all_product"
+import axios from "axios"
 
 export const ShopContext = createContext(null)
 
 const getDefaultCart = () => {
     let cart = {}
-    for (let index = 0; index < all_product.length + 1; index++) {
+    for (let index = 0; index < 300 + 1; index++) {
         cart[index] = 0
     }
     return cart
 }
 
+let all_product = []
+
 const ShopContextProvider = (props) => {
-    const [cartItems, setCartItems] = useState(getDefaultCart());
+    const [cartItems, setCartItems] = useState(getDefaultCart())
+    const [all_product, setAll_Product] = useState([])
+
+    const fetchData = async () => {
+        try {
+            const response = await axios.get("http://localhost:4000/allproducts")
+            const data = response.data
+            setAll_Product(data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
     const addToCart = (itemId) => {
         setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }))
     }
@@ -22,16 +41,15 @@ const ShopContextProvider = (props) => {
 
     const getTotalCartAmount = () => {
         let totalAmount = 0
-        let totalCartItems = 0;
+        let totalCartItems = 0
         for (const item in cartItems) {
             if (cartItems[item] > 0) {
-                totalCartItems += cartItems[item] 
+                totalCartItems += cartItems[item]
                 let itemInfo = all_product.find((product) => product.id === Number(item))
                 totalAmount += itemInfo.new_price * cartItems[item]
-                
             }
         }
-        return {totalAmount, totalCartItems}
+        return { totalAmount, totalCartItems }
     }
 
     const contextValue = { getTotalCartAmount, all_product, cartItems, addToCart, removeFromCart }
