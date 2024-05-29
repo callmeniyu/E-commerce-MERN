a16c5937cc86acb8f777b18b662e58302420396f
Mohammed Niyas <getniyashere@gmail.com>
2024-05-29 13:45:00 +0530

    added cors configuration from frontend for production

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,10 @@ const app = express()
 
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
+
+app.use(cors({
+  origin: 'https://e-commerce-mern-frontend-sand.vercel.app'
+}));
 
 app.use(
     cors({
